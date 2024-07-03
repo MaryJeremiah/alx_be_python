e0ddd836201a7455091ec6879eaef2238fa45ec8
Mary Jeremiah <jmarymwende@gmail.com>
2024-07-03 19:30:46 +0300

    weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -8,12 +8,11 @@ weather = input("What's the weather like today? (sunny/rainy/cold): ").strip().l
 
 
 #provide clothing recommendations based on the weather conditions
-
-if 'weather' =="sunny": # type: ignore
+if 'weather' =="sunny":
         print( "Wear a t-shirt and sunglasses.")
-elif 'weather' == "rainny":
+elif 'weather' == "rainy":
         print("Don't forget your umbrella and a raincoat.")
-elif 'weather' == "cold": # type: ignore
+elif 'weather' == "cold":
         print("Make sure to wear a warm coat and a scarf.")
 else:
         print("Sorry, I don't have recommendations for this weather.")
