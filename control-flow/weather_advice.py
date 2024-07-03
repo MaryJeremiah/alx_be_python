727aeb6914ee3550e2eb2a3c53f7c270ee23f0a3
Mary Jeremiah <jmarymwende@gmail.com>
2024-07-03 19:14:30 +0300

    weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,14 @@
+
+
+# Prompt the user for task details
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").strip().lower()
+time_bound = input("Is it time-bound? (yes/no): ").strip().lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
+
+
 #provide clothing recommendations based on the weather conditions
+
 if 'weather' =="sunny": # type: ignore
         print( "Wear a t-shirt and sunglasses.")
 elif 'weather' == "rainny":
